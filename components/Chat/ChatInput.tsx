b83390ae127b62fee42907249cb9035a795c7b2c
Adnane <adnanesaber15@gmail.com>
2024-11-12 03:25:45 +0900

    KAL-25: Integrate chatbot API, update UI components, and manage state for weather queries

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import { AppDispatch, RootState } from "@/store";
+import { handleMessage } from "@/store/slices/chatbotSlide";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const ChatInput = () => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const dispatch = useDispatch<AppDispatch>();
+  const loading = useSelector((state: RootState) => state.chatbot.loading);
+  const handleKeyDown = async (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ): Promise<void> => {
+    if (event.key === "Enter") {
+      dispatch(handleMessage(inputValue));
+      setInputValue("");
+    }
+  };
   return (
     <div className="focus-within:before:opacity-80 focus-within:after:opacity-50 neonglow relative flex items-center justify-center w-full">
       <div className="kalopsai-input-icon"></div>
       <input
         type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={handleKeyDown}
+        disabled={loading}
         placeholder="Ask Kalopsai..."
         className="neon-input w-full relative z-10 pl-14 pr-6 py-3 rounded-full bg-gray-600/70 text-white outline-none shadow-lg placeholder:text-gray-300 placeholder:font-light placeholder:text-base"
       />
--- types/weather.ts
+++ types/weather.ts
@@ -1,46 +1,62 @@
+// Weather object structure used in multiple types
+type WeatherInfo = {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+};
+
+type WindInfo = {
+  speed: number;
+  deg: number;
+  gust: number;
+};
+
+type MainWeatherData = {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  humidity: number;
+  sea_level: number;
+  grnd_level: number;
+  temp_kf: number;
+};
+
+type CloudsInfo = {
+  all: number;
+};
+
+type SysInfo = {
+  type: number;
+  id: number;
+  country: string;
+  sunrise: number;
+  sunset: number;
+};
+
+type Coordinates = {
+  lon: number;
+  lat: number;
+};
+
 export type ILocationWeatherResponse = {
-  coord: {
-    lon: number;
-    lat: number;
-  };
-  weather: Array<{
-    id: number;
-    main: string;
-    description: string;
-    icon: string;
-  }>;
+  coord: Coordinates;
+  weather: WeatherInfo[];
   base: string;
-  main: {
-    temp: number;
-    feels_like: number;
-    temp_min: number;
-    temp_max: number;
-    pressure: number;
-    humidity: number;
-    sea_level: number;
-    grnd_level: number;
-  };
+  main: MainWeatherData;
   visibility: number;
-  wind: {
-    speed: number;
-    deg: number;
-  };
-  clouds: {
-    all: number;
-  };
+  wind: WindInfo;
+  clouds: CloudsInfo;
   dt: number;
-  sys: {
-    type: number;
-    id: number;
-    country: string;
-    sunrise: number;
-    sunset: number;
-  };
+  sys: SysInfo;
   timezone: number;
   id: number;
   name: string;
   cod: number;
 };
+
 export type ICityCoordsResponse = {
   name: string;
   local_names: Record<string, string>;
@@ -58,4 +74,28 @@ export type IWeatherMapper = {
 
 export type WeatherData = {
   [key: number]: IWeatherMapper;
+};
+
+export type WeatherMessageResponse = {
+  response: string;
+  data: {
+    cod: string;
+    message: number;
+    cnt: number;
+    city: string;
+    day_data: {
+      dt: number;
+      main: MainWeatherData;
+      weather: WeatherInfo[];
+      clouds: CloudsInfo;
+      wind: WindInfo;
+      visibility: number;
+      pop: number;
+      sys: {
+        pod: string;
+      };
+      dt_txt: string;
+    };
+    lang: string;
+  };
 };
